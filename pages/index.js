35d0f0eb1bb12b65eaa779d8e676300063013bf9
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-09 19:55:20 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,37 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className={styles.all}>
-    <div className={styles.container}>
-      <Head>
-        <title>MrJokester</title>
-        <meta name="description" content="My Website" />
-        <link rel="icon" href="public/profile.png" />
-      </Head>
+      <div className={styles.container}>
+        <Head>
+          <title>MrJokester</title>
+          <meta name="description" content="My Website" />
+          <link rel="icon" href="/profile.png" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          <Image src="/title.png" alt="Profile Icon" width={40} height={40} /> BDFD Snippets</h1>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            <Image src="/title.png" alt="Profile Icon" width={40} height={40} /> MrJokester
+          </h1>
 
-        <div className={styles.grid}>
-          <a href="https://3621d689-8f08-41be-9cfd-27404c776dae-00-1y9ohzghjd8xs.spock.replit.dev/bdfd-bomb" className={styles.card}>
-            <h2>Bomb Game</h2>
-          </a>
-        </div>
-      </main>
-    </div>
+          <div className={styles.grid}>
+            <a href="https://3621d689-8f08-41be-9cfd-27404c776dae-00-1y9ohzghjd8xs.spock.replit.dev/bdfd" className={styles.card}>
+              <h2>BDFD Snippets</h2>
+            </a>
+
+            <a href="https://3621d689-8f08-41be-9cfd-27404c776dae-00-1y9ohzghjd8xs.spock.replit.dev/me" className={styles.card}>
+              <h2>About Me</h2>
+            </a>
+
+            <a href="https://discord.com/oauth2/authorize?client_id=1251525661185806336" className={styles.card}>
+              <h2>My Discord Bot</h2>
+            </a>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
