972c2c8473ef566129c09f2ad076e118bf026ce6
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-10 18:41:26 +0000

    changes

--- pages/bdfd.js
+++ pages/bdfd.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-cconst Home = () => {
+const Home = () => {
   return (
     <div className={styles.all}>
       <div className={styles.container}>
