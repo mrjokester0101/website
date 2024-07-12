930b909603cc4bd70ae8dacf4077a92b508c7cba
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-12 18:17:59 +0300

    Create bdfd-user.js

---
+++ pages/bdfd-user.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import CopyTextButton from "../components/CopyTextButton";
+
+const Home = () => {
+  return (
+    <div className={styles.all}>
+      <div className={styles.container}>
+        <Head>
+          <title>MrJokester</title>
+          <meta name="description" content="My Website" />
+          <link rel="icon" href="/profile.png" />
+        </Head>
+
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            <Image src="/title.png" alt="Profile Icon" width={40} height={40} /> ChatBot
+          </h1>
+
+          <div className={styles.snippet}>
+            <p className={styles.snippet}>Main Code</p>
+            <code className={styles.code}>Code Here</code>
+     <CopyTextButton text={`Code Here`}/>
+          </div>
+
+          <div className={styles.grid}>
+            <a href="https://mrjokester.netlify.app/bdfd" className={styles.card}>
+              <h2>BDFD Snippets</h2>
+            </a>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
