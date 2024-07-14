a9ebfbd6433abf54f2286efe8a096cff69968243
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-14 17:34:42 +0300

    Update bdfd-user.js

--- pages/bdfd-user.js
+++ pages/bdfd-user.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../pages/bdfd/Home.module.css";
-import CopyTextButton from "../pages/bdfd/CopyTextButton";
+import styles from "../styles/Home.module.css";
+import CopyTextButton from "../components/bdfd/CopyTextButton";
 
 const Home = () => {
   return (
