5354b06eb3fa2d48473d2b3aef3f7f7a0bd55dfc
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-17 22:37:24 +0300

    Update bdfd-user.js

--- pages/bdfd-user.js
+++ pages/bdfd-user.js
@@ -17,7 +17,8 @@ const Home = () => {
           <h1 className={styles.title}>
             <Image className={styles.image} src="/title.png" alt="Profile Icon"/>
           <h1 className={styles.title}>User Info</h1>
-
+          </h1>
+  
           <div className={styles.snippet}>
             <p className={styles.snippet}>Main Code</p>
             <code className={styles.code}>{`$nomention
