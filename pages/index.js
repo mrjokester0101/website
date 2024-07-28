bf2e528c8e43b6aa6775644032ea15ee31fcddcb
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-28 18:54:50 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,18 +21,24 @@ const Home = () => {
   
           <div className={styles.grid}>
             <h2 className={styles.card}>BDFD Snippets</h2>
-            <a href="https://mrjokester.netlify.app/bdfd">
+            <a href="https://mrjokester.netlify.app/bdfd" className={styles.card}>
               <p>See Codes</p>
             </a>
+          </div>
 
+          <div className={styles.grid}>
             <a href="https://mrjokester.netlify.app/me" className={styles.card}>
               <h2>About Me</h2>
             </a>
-
+  </div>
+  
+<div className={styles.grid}>
             <a href="https://discord.com/oauth2/authorize?client_id=1251525661185806336" className={styles.card}>
               <h2>Doors Moderator Bot</h2>
             </a>
+  </div>
 
+  <div className={styles.grid}>
              <a href="https://discord.com/invite/VWEcYvKztc" className={styles.card}>
               <h2>MrJokester Studios</h2>
             </a>
