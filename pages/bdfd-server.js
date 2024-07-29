4482fe374a813790bd93fc09b6cd8b4eefca65e4
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-29 11:34:41 +0300

    BDFD Server Info Snippet
    Updated title
    Added description

--- pages/bdfd-server.js
+++ pages/bdfd-server.js
@@ -16,7 +16,8 @@ const Home = () => {
         <main className={styles.main}>
           <h1 className={styles.title}>
             <Image className={styles.image} src="/title.png" alt="Profile Icon"/>
-          <h1 className={styles.title}>Server Info</h1>
+          <h1 className={styles.title}>MrJokester</h1>
+          <p className={styles.description}>Server Info</p>
           </h1>
   
           <div className={styles.snippet}>
