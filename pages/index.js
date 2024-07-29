be492e511c986f949f86f45bedbfc29e8469e41d
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-29 10:03:05 +0300

    Main Page
    Updated buttons

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,14 @@ const Home = () => {
           <meta name="description" content="My Website" />
           <link rel="icon" href="/profile.png" />
         </Head>
-
         <main className={styles.main}>
           <h1 className={styles.title}>
             <Image className={styles.image} src="/title.png" alt="Profile Icon"/>
           <h1 className={styles.title}>MrJokester</h1>
           <p className={styles.description}>Main Page</p>
           </h1>
-  
+
+        <div className={styles.grid}>
           <div className={styles.card}>
             <h2>BDFD Snippets</h2>
             <a href="https://mrjokester.netlify.app/bdfd" className={styles.card1}>
@@ -26,23 +26,28 @@ const Home = () => {
             </a>
           </div>
 
-          <div className={styles.grid}>
-            <a href="https://mrjokester.netlify.app/me" className={styles.card}>
-              <h2>About Me</h2>
-            </a>
-  </div>
-  
-<div className={styles.grid}>
-            <a href="https://discord.com/oauth2/authorize?client_id=1251525661185806336" className={styles.card}>
-              <h2>Doors Moderator Bot</h2>
-            </a>
-  </div>
-
-  <div className={styles.grid}>
-             <a href="https://discord.com/invite/VWEcYvKztc" className={styles.card}>
-              <h2>MrJokester Studios</h2>
+          <div className={styles.card}>
+            <h2>About Me</h2>
+            <a href="https://mrjokester.netlify.app/me" className={styles.card1}>
+              <p>Click Me</p>
             </a>
           </div>
+  
+          <div className={styles.card}>
+            <h2>Doors Moderator Bot</h2>
+            <a href="https://discord.com/oauth2/authorize?client_id=1251525661185806336" className={styles.card1}>
+              <p>Get Invite</p>
+            </a>
+          </div>
+
+          <div className={styles.card}>
+            <h2>MrJokester Studios
+            <a href="https://discord.com/invite/VWEcYvKztc" className={styles.card1}>
+              <p>Get Invite</p>
+            </a>
+          </div>
+        </div>
+
         </main>
       </div>
     </div>
