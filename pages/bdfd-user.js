af006947b32d98da0e6aa0a4b7521d99240011c2
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-31 18:59:50 +0300

    BDFD User Info Snippet
    Updated button

--- pages/bdfd-user.js
+++ pages/bdfd-user.js
@@ -98,7 +98,7 @@ $color[$random[0;999999]]`}/>
           <div className={styles.grid}>
             <div className={styles.card}>
             <h2>BDFD Snippets</h2>
-            <a href="https://mrjokester.netlify.app/bdfd" className={styles.card1}>
+            <a href="https://mrjokester.netlify.app/bdfd" className={styles.linkcard}>
               <p>See Codes</p>
             </a>
             </div>
