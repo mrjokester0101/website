5c6bf14b66a5527b5035cc9de68dabd6c9c79d78
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-31 21:52:27 +0300

    BDFD Basic Economy 4 in 1 Snippet
    Updated link

--- pages/bdfd-eco-4in1.js
+++ pages/bdfd-eco-4in1.js
@@ -205,7 +205,7 @@ $endif`}/>
           <div className={styles.grid}>
             <div className={styles.card}>
             <h2>BDFD Snippets</h2>
-            <a href="https://mrjokester.netlify.app/bdfd" className={styles.linkcard}>
+            <a href="https://mrjokester.vercel.app/bdfd" className={styles.linkcard}>
               <p>See Codes</p>
             </a>
             </div>
