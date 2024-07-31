03138ff5a22e9020c17b78f7271aa1570ce5ec0c
mrjokester0101 <mrjokester0101@gmail.com>
2024-07-31 18:08:38 +0300

    BDFD Snippets
    Fixed bugs

--- pages/bdfd.js
+++ pages/bdfd.js
@@ -13,52 +13,50 @@ const Home = () => {
         </Head>
 
         <main className={styles.main}>
-          <h1 className={styles.title}>
-            <Image className={styles.image} src="/title.png" alt="Profile Icon"/>
+          <Image className={styles.image} src="/title.png" alt="Profile Icon"/>
           <h1 className={styles.title}>MrJokester</h1>
           <p className={styles.description}>BDFD Snippets</p>
-          </h1>
   
-       <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>{`Bomb Game`}</h2>
-            <a href="https://mrjokester.netlify.app/bdfd-bomb" className={styles.card1}>
-              <p>Get Code</p>
-            </a>
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <h2>Bomb Game</h2>
+              <a href="https://mrjokester.netlify.app/bdfd-bomb" className={styles.card1}>
+                <p>Get Code</p>
+              </a>
+            </div>
+
+            <div className={styles.card}>
+              <h2>ChatBot</h2>
+              <a href="https://mrjokester.netlify.app/bdfd-chat" className={styles.card1}>
+                <p>Get Code</p>
+              </a>
+            </div>
+          </div>
+  
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <h2>User Info</h2>
+              <a href="https://mrjokester.netlify.app/bdfd-user" className={styles.card1}>
+                <p>Get Code</p>
+              </a>
+            </div>
+  
+            <div className={styles.card}>
+              <h2>Server Info</h2>
+              <a href="https://mrjokester.netlify.app/bdfd-server" className={styles.card1}>
+                <p>Get Code</p>
+              </a>
+            </div>
           </div>
 
-          <div className={styles.card}>
-            <h2>{`ChatBot`}</h2>
-            <a href="https://mrjokester.netlify.app/bdfd-chat" className={styles.card1}>
-              <p>Get Code</p>
-            </a>
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <h2>Basic Economy 4 in 1 Commands</h2>
+              <a href="https://mrjokester.netlify.app/bdfd-eco-4in1" className={styles.card1}>
+                <p>Get Code</p>
+              </a>
+            </div>
           </div>
-        </div>
-  
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>{`User Info`}</h2>
-            <a href="https://mrjokester.netlify.app/bdfd-user" className={styles.card1}>
-              <p>Get Code</p>
-            </a>
-          </div>
-  
-          <div className={styles.card}>
-            <h2>{`Server Info`}</h2>
-            <a href="https://mrjokester.netlify.app/bdfd-server" className={styles.card1}>
-              <p>Get Code</p>
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>{`Basic Economy 4 in 1 Commands`}</h2>
-            <a href="https://mrjokester.netlify.app/bdfd-eco-4in1" className={styles.card1}>
-              <p>Get Code</p>
-            </a>
-          </div>
-        </div>
         </main>
       </div>
     </div>
