8bdb06c6456aad98eca34a0901ecfc2482d8278d
mrjokester0101 <mrjokester0101@gmail.com>
2024-08-01 16:58:27 +0300

    BDFD Snippets
    Added bracket

--- pages/bdfd.js
+++ pages/bdfd.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import SpeedInsights from "../@vercel/speed-insights/next";
 
 const Home = () => {
   return (
+    <SpeedInsights>
     <div className={styles.all}>
       <div className={styles.container}>
         <Head>
@@ -56,6 +58,7 @@ const Home = () => {
         </main>
       </div>
     </div>
+  </SpeedInsights>
   );
 };
 
