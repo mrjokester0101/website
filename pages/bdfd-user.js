069f0cb674c0f35e30766e2391d3832e41fd1149
mrjokester0101 <mrjokester0101@gmail.com>
2024-08-01 16:57:36 +0300

    BDFD User Info Snippet
    Updated bracket

--- pages/bdfd-user.js
+++ pages/bdfd-user.js
@@ -2,9 +2,11 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import CopyTextButton from "../components/CopyTextButton";
+import SpeedInsights from "../@vercel/speed-insights/next";
 
 const Home = () => {
   return (
+    <SpeedInsights>
     <div className={styles.all}>
       <div className={styles.container}>
         <Head>
@@ -106,6 +108,7 @@ $color[$random[0;999999]]`}/>
         </main>
       </div>
     </div>
+  </SpeedInsights>
   );
 };
 
