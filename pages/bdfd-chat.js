63f778c760c9bce65fbc6bd59f0571c7d82d31ee
mrjokester0101 <mrjokester0101@gmail.com>
2024-08-01 20:34:49 +0300

    Removed tag

--- pages/bdfd-chat.js
+++ pages/bdfd-chat.js
@@ -2,11 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import CopyTextButton from "../components/CopyTextButton";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const Home = () => {
   return (
-    <SpeedInsights>
     <div className={styles.all}>
       <div className={styles.container}>
         <Head>
@@ -80,7 +78,6 @@ $endif`}/>
         </main>
       </div>
     </div>
-  </SpeedInsights>
   );
 };
 
