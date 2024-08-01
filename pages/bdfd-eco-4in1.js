ae5c942b46c7076bd3bbde0a3ef68edcc53e9d6a
mrjokester0101 <mrjokester0101@gmail.com>
2024-08-01 20:34:16 +0300

    BDFD Basic Economy 4 in 1 Snippet
    Removed tag

--- pages/bdfd-eco-4in1.js
+++ pages/bdfd-eco-4in1.js
@@ -2,11 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import CopyTextButton from "../components/CopyTextButton";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const Home = () => {
   return (
-    <SpeedInsights>
     <div className={styles.all}>
       <div className={styles.container}>
         <Head>
@@ -215,7 +213,6 @@ $endif`}/>
         </main>
       </div>
     </div>
-  </SpeedInsights>
   );
 };
 
