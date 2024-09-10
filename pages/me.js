90efd894050d65d7fc8137f8b9d17359ba6723a2
mrjokester0101 <mrjokester0101@gmail.com>
2024-09-10 18:09:57 +0300

    Update me.js

--- pages/me.js
+++ pages/me.js
@@ -14,9 +14,9 @@ const Home = () => {
 
 
         <main className={styles.main}>
-            <Image className={styles.image} src="/title.png" alt="Profile Icon">
-               <h1 className={styles.title}>MrJokester</h1>
-            </Image>
+            <h1 className={styles.title}>
+               <Image className={styles.image} src="/title.png" alt="Profile Icon"/>MrJokester
+            </h1>
           <p className={styles.description}>About Me</p>
 
         <div className={styles.buttons}>
