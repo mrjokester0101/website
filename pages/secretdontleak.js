c79fc951f860bfaa4460f37f2aab62e47719c214
mrjokester0101 <mrjokester0101@gmail.com>
2024-09-16 13:05:45 +0300

    Create secretdontleak.js

---
+++ pages/secretdontleak.js
@@ -0,0 +1,10 @@
+import UserProfile from '../components/UserProfile';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the User Profile Page</h1>
+      <UserProfile />
+    </div>
+  );
+}
