08dd59c7a9da63b2f3091098b777ea8fdbf6b6c0
mrjokester0101 <mrjokester0101@gmail.com>
2024-09-19 21:44:44 +0300

    Update UserProfile.js

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -12,14 +12,16 @@ const UserProfile = () => {
       <button className={styles.profileButton} onClick={toggleProfile}>
         <Image
           src="/discord_label.png"
+          width="50"
+          height="50"
           alt={isOpen ? 'Hide Profile' : 'Show Profile'}
-          width={50}
-          height={50}
           className={styles.discordIcon}
         />
       </button>
 
       <div className={`${styles.profileDetails} ${isOpen ? styles.show : styles.hide}`}>
+        <h2 className={styles.avatarBackground}>‎ 
+        </h2>
         <Image
           src="/profile.png"
           alt="Profile Avatar"
@@ -29,10 +31,15 @@ const UserProfile = () => {
         />
         <div className={styles.userInfo}>
           <h2 className={styles.userName}>MrJokester</h2>
+          <p>mrjokester0101</p>
+          <p>He/Him</p>
+          <div className={styles.roleBackground}>
           <div className={styles.userRoles}>
             <span className={`${styles.userRole} ${styles.roleCoder}`}>Coder</span>
             <span className={`${styles.userRole} ${styles.roleDesigner}`}>Designer</span>
           </div>
+          </div>
+          <p>Member Since: Jun 15, 2024</p>
         </div>
       </div>
     </div>
